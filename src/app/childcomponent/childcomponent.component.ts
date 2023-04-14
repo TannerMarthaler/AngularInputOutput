9613252f9f994cae682c1a797ae619cc4cb0ea3c
TannerMarthaler <125493123+TannerMarthaler@users.noreply.github.com>
2023-04-14 11:51:04 -0500

    3 files modified

--- src/app/childcomponent/childcomponent.component.ts
+++ src/app/childcomponent/childcomponent.component.ts
@@ -1,19 +1,14 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
-
 @Component({
   selector: 'app-childcomponent',
   templateUrl: './childcomponent.component.html',
-  styleUrls: ['./childcomponent.component.css']
+  styleUrls: ['./childcomponent.component.css'],
 })
 export class ChildcomponentComponent implements OnInit {
+  @Input() inputText: string = '';
 
+  constructor() {}
 
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-
+  ngOnInit() {}
 }
